58d4ecb4ce1f32f021fd9d389d023929b9c32343
Ataki_pw11@itstep.edu.az <Ataki_pw11@itstep.edu.az>
2023-02-07 17:16:20 +0400

    Final

--- app12/Form1.Designer.cs
+++ app12/Form1.Designer.cs
@@ -29,7 +29,8 @@
         private void InitializeComponent()
         {
             this.timeLbl = new System.Windows.Forms.Label();
-            this.LondonBtn = new System.Windows.Forms.Button();
+            this.londonBtn = new System.Windows.Forms.Button();
+            this.bakuBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // timeLbl
@@ -40,22 +41,35 @@
             this.timeLbl.ForeColor = System.Drawing.Color.DodgerBlue;
             this.timeLbl.Location = new System.Drawing.Point(127, 9);
             this.timeLbl.Name = "timeLbl";
-            this.timeLbl.Size = new System.Drawing.Size(96, 72);
+            this.timeLbl.Size = new System.Drawing.Size(217, 72);
             this.timeLbl.TabIndex = 0;
-            this.timeLbl.Text = "...";
+            this.timeLbl.Text = "Time";
             // 
-            // LondonBtn
+            // londonBtn
             // 
-            this.LondonBtn.BackColor = System.Drawing.Color.Transparent;
-            this.LondonBtn.Font = new System.Drawing.Font("ROG Fonts", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LondonBtn.ForeColor = System.Drawing.Color.Navy;
-            this.LondonBtn.Location = new System.Drawing.Point(700, 558);
-            this.LondonBtn.Name = "LondonBtn";
-            this.LondonBtn.Size = new System.Drawing.Size(332, 65);
-            this.LondonBtn.TabIndex = 1;
-            this.LondonBtn.Text = "London";
-            this.LondonBtn.UseVisualStyleBackColor = false;
-            this.LondonBtn.MouseClick += new System.Windows.Forms.MouseEventHandler(this.LondonBtn_MouseClick);
+            this.londonBtn.BackColor = System.Drawing.Color.Transparent;
+            this.londonBtn.Font = new System.Drawing.Font("ROG Fonts", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.londonBtn.ForeColor = System.Drawing.Color.Navy;
+            this.londonBtn.Location = new System.Drawing.Point(700, 558);
+            this.londonBtn.Name = "londonBtn";
+            this.londonBtn.Size = new System.Drawing.Size(332, 65);
+            this.londonBtn.TabIndex = 1;
+            this.londonBtn.Text = "London";
+            this.londonBtn.UseVisualStyleBackColor = false;
+            this.londonBtn.MouseClick += new System.Windows.Forms.MouseEventHandler(this.londonBtn_MouseClick);
+            // 
+            // bakuBtn
+            // 
+            this.bakuBtn.BackColor = System.Drawing.Color.Transparent;
+            this.bakuBtn.Font = new System.Drawing.Font("ROG Fonts", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bakuBtn.ForeColor = System.Drawing.Color.Navy;
+            this.bakuBtn.Location = new System.Drawing.Point(81, 558);
+            this.bakuBtn.Name = "bakuBtn";
+            this.bakuBtn.Size = new System.Drawing.Size(332, 65);
+            this.bakuBtn.TabIndex = 2;
+            this.bakuBtn.Text = "BAKU";
+            this.bakuBtn.UseVisualStyleBackColor = false;
+            this.bakuBtn.Click += new System.EventHandler(this.bakuBtn_Click);
             // 
             // Form1
             // 
@@ -63,7 +77,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::app12.Properties.Resources.img_119157_baku_flame_tower;
             this.ClientSize = new System.Drawing.Size(1181, 727);
-            this.Controls.Add(this.LondonBtn);
+            this.Controls.Add(this.bakuBtn);
+            this.Controls.Add(this.londonBtn);
             this.Controls.Add(this.timeLbl);
             this.MaximumSize = new System.Drawing.Size(10000, 1000);
             this.Name = "Form1";
@@ -76,7 +91,8 @@
         #endregion
 
         private System.Windows.Forms.Label timeLbl;
-        private System.Windows.Forms.Button LondonBtn;
+        private System.Windows.Forms.Button londonBtn;
+        private System.Windows.Forms.Button bakuBtn;
     }
 }
 
--- app12/Form1.cs
+++ app12/Form1.cs
@@ -15,41 +15,35 @@ namespace app12
         public Form1()
         {
             InitializeComponent();
-            Timer timer = new Timer();
             timer.Interval = 1000;
-            timer.Tick += Timer_Tick;
-
+            timer.Tick += BakuTimer_Tick;
             timer.Start();
+
         }
 
-        Button button1 = new Button();
 
-        private void Timer_Tick(object sender, EventArgs e)
+        Timer timer = new Timer();
+
+        private void BakuTimer_Tick(object sender, EventArgs e)
         {
             timeLbl.Text = DateTime.Now.ToLongTimeString();
         }
 
-        private void LondonBtn_MouseClick(object sender, MouseEventArgs e)
+        private void LondonTime_Tick(object sender, EventArgs e)
         {
-            LondonBtn.Dispose();
-            timeLbl.Dispose();
-            button1.Name = "BakuBtn";
-            button1.Text = "BAKU";
-            button1.Font = new Font("ROG Fonts", 28, FontStyle.Bold);
-            Random random = new Random();
-            button1.Location = new Point(80, 450);
-            button1.ForeColor = Color.Navy;
-            button1.AutoSize = true;
-            button1.MouseClick += LondonBtn_MouseClick;
-            this.Controls.Add(button1);
-            this.BackgroundImage = Properties.Resources.londn;
             timeLbl.Text = DateTime.Now.AddHours(4).ToLongTimeString();
         }
 
-        private void BakuBtn_MauseClick(object sender, EventArgs e)
+        private void londonBtn_MouseClick(object sender, MouseEventArgs e)
         {
-            
+            timer.Tick += LondonTime_Tick;
+            this.BackgroundImage = Properties.Resources.londn;
         }
 
+        private void bakuBtn_Click(object sender, EventArgs e)
+        {
+            timer.Tick += BakuTimer_Tick;
+            this.BackgroundImage = Properties.Resources.img_119157_baku_flame_tower;
+        }
     }
 }
